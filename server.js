f9a1578fe74e225b3af9f54c5b6b795314d98277
lukereiner <github@mightymail.net>
2026-05-06 21:09:23 -0400

    initial express server set up, gitignore file

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello there!')
+})
+
+const startServer = async() => {
+    app.listen(PORT, () => {
+        console.log(`Server listening on PORT ${PORT}`);
+    });
+};
+
+startServer();
