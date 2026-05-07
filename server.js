ccc06f84640c6bef1dcbb7883559414c50ceb1b4
lukereiner <github@mightymail.net>
2026-05-06 22:20:44 -0400

    move variables to env file, install dotenv

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
+require('dotenv').config()
+const { PORT } = require('./config')
 
 app.get('/', (req, res) => {
     res.send('Hello there!')
