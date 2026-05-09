b363290767265e599fb3d6854d3b76a486c174dc
lukereiner <github@mightymail.net>
2026-05-08 23:01:30 -0400

    reorganized files into routes, services models. Create usersModel for db statements

---
+++ models/usersModel.js
@@ -0,0 +1,56 @@
+const db = require("../db/myPool");
+const pgp = require("pg-promise")({ capSQL: true });
+
+module.exports = class UserModel {
+  // create new user record
+  async create(data) {
+    try {
+      // Generate SQL statement
+      const statement = pgp.helpers.insert(data, null, "users") + "RETURNING *";
+
+      // Execute SQL statement
+      const result = await db.query(statement);
+
+      // Ensure if rows are undefined or zero, doesn't crash server
+      if (result.rows?.length) {
+        return result.rows[0];
+      }
+
+      return null;
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
+
+  // Update a user record
+  async update(data) {
+    try {
+      const { id, ...params } = data;
+
+      // Generate SQL Statement to update
+      const condition = pgp.as.format("WHERE id = ${id} RETURNING *", { id });
+      const statement = pgp.helpers.update(params, null, "users") + condition;
+
+      // EXECUTE SQL STATEMENT
+      const result = await db.query(statement);
+
+      // Ensure if rows are undefined or zero, doesn't crash server
+      if (result.rows?.length) {
+        return result.rows[0];
+      }
+
+      return null;
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
+
+  // Find user record by email
+  async findUserByEmail(email) {
+    try {
+        const statement // stopped here 23:00 5.8.26
+    } catch(err) {
+        throw new Error(err);
+    }
+  }
+};
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Pool } = require('pg');
-
-const pool = new Pool({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT
-});
+const pool = require('../db/myPool')
 
 router.get('/', async (req, res) => {
     try {
