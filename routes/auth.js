997b799d0602fa3e535295f8c5d13a48e064a14e
lukereiner <github@mightymail.net>
2026-06-30 22:41:24 -0400

    add logout route - to be tested

--- routes/auth.js
+++ routes/auth.js
@@ -40,4 +40,24 @@ module.exports = (app) => {
       user: cleanUser,
     });
   });
+
+  router.post('/logout', (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+
+        req.session.destroy((err) => {
+            if (err) {
+                return next(err);
+            }
+
+            res.clearCookie('connect.sid'); // Default express-session cookie name
+
+            return res.status(200).json({
+                message: "Logged out successfully"
+            })
+        })
+    })
+  })
 };
